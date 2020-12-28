e2867144f9a4f56fb35d898e6576c44656cad045
pettun0104 <pettun0104@gmail.com>
2020-12-28 13:45:30 +0300

    m

--- untitled-1.py
+++ untitled-1.py
@@ -3,14 +3,18 @@ from flask import Flask
 app = Flask(__name__)
 
 d = {}
-@app.route('/stats/<short_url>')
+@app.route('/stats/<short_url>/')
 def f(short_url):
     global d
     if short_url not in d.keys():
         d[short_url] = 1
     else:
         d[short_url] += 1
-    return str(d[short_url])
+    if d[short_url] in [2, 3, 4]:
+        s = 'ѕользователи проходили по этой ссылке ' + str(d[short_url]) + ' раза'
+    else:
+        s = 'ѕользователи проходили по этой ссылке ' + str(d[short_url]) + ' раз'
+    return s
 
 if __name__ == "__main__":
     app.run()
